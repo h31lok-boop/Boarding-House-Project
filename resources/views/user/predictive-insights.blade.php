b66c5caf5a6981d062260aff191cf1a42bd3f816
Jay Jemino Ababon <jayjeminoababon@gmail.com>
2026-08-13 10:45:05 +0800

    refactor: remove tenant insights view

--- resources/views/user/predictive-insights.blade.php
+++
@@ -1,5 +0,0 @@
-<x-layouts.dashboard>
-    <x-user.shell search-placeholder="Search boarding houses...">
-        @include('shared.predictive-insights-content')
-    </x-user.shell>
-</x-layouts.dashboard>
