c47b1d3dd0fd26e5813b776b4ef36752b6bb8239
Jay Jemino Ababon <jayjeminoababon@gmail.com>
2026-08-13 10:09:35 +0800

    ui: update predictive-insights.blade view

---
+++ resources/views/user/predictive-insights.blade.php
@@ -0,0 +1,5 @@
+<x-layouts.dashboard>
+    <x-user.shell search-placeholder="Search boarding houses...">
+        @include('shared.predictive-insights-content')
+    </x-user.shell>
+</x-layouts.dashboard>
