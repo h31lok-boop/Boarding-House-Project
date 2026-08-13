040c0f06c29574a8918fc67ac4748a2a1fd3888a
Jay Jemino Ababon <jayjeminoababon@gmail.com>
2026-08-13 10:09:35 +0800

    ui: update dashboard.blade view

--- resources/views/components/layouts/dashboard.blade.php
+++ resources/views/components/layouts/dashboard.blade.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
-<html lang="en" data-theme="light">
+<html lang="en" data-theme="light" data-theme-mode="dashboard">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Boarding House Match Making System') }}</title>
-    <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
+    <link rel="icon" type="image/png" href="{{ asset('images/boardmatch-final-logo.png') }}">
     <script>
         (function () {
             try {
