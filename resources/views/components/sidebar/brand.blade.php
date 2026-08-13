0c8e75d51eef63b58edc2517ff7582deab980c94
Jay Jemino Ababon <jayjeminoababon@gmail.com>
2026-08-13 10:09:35 +0800

    ui: update brand.blade view

--- resources/views/components/sidebar/brand.blade.php
+++ resources/views/components/sidebar/brand.blade.php
@@ -10,8 +10,8 @@
 @endphp
 
 <a href="{{ route($dashRoute) }}" class="user-sidebar-brand flex min-w-0 flex-1 items-center gap-2.5">
-    <div class="sidebar-brand-icon flex h-9 w-9 shrink-0 items-center justify-center rounded-xl bg-blue-600 text-white shadow-[0_10px_22px_rgba(37,99,235,0.3)] ring-1 ring-white/15">
-        @include('components.sidebar.partials.admin-icon', ['name' => 'boarding-house'])
+    <div class="sidebar-brand-icon h-9 w-9 shrink-0 overflow-hidden rounded-xl shadow-[0_10px_22px_rgba(37,99,235,0.3)] ring-1 ring-white/15">
+        <img src="{{ asset('images/boardmatch-final-logo.png') }}" alt="" class="h-full w-full object-cover" aria-hidden="true">
     </div>
     <div class="sidebar-brand-text min-w-0 leading-tight">
         <p class="truncate text-lg font-bold tracking-tight text-white">BoardMatch</p>
